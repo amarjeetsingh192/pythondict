9c471ce292ad9605ea8063cda181041953d2fd65
amar <amarjeet192175singh@gmail.com>
2023-02-22 14:07:41 +0530

    intail

---
+++ distremove.py
@@ -0,0 +1,13 @@
+# Initializing dictionary
+test_dict = {"Arushi": 22, "Mani": 21, "Haritha": 21}
+
+# Printing dictionary before removal
+print("The dictionary before performing remove is : ", test_dict)
+
+# Using del to remove a dict
+
+del test_dict['Mani']
+
+# Printing dictionary after removal
+print("The dictionary after remove is : ", test_dict)
+
